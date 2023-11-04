090674b8001a6c8475123cc01566e36da1f0dc28
VinayYadava <vinay.yadav4501@gmail.com>
2023-11-04 14:47:00 +0530

    fixed routing made separate components directory and corrected span hover color as background

---
+++ app/About/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function About(){
+  return (
+    <div className="screen">hello</div>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
-import PageHeader from './components/Header/PageHeader'
-import Experience from './components/Experience/Experience'
-import AppHomeSection from './components/AppHome/AppHomeContent'
+import PageHeader from '../components/Header/PageHeader'
+import AppHomeSection from '../components/AppHome/AppHomeContent'
 export default function Home() {
   return (
     <div className="screen">
       <PageHeader />
       <AppHomeSection  />
-      
     </div>
   )
 }
